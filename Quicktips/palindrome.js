80517e5324828c4856fb2dda4d8eedbdd48d5da6
Rohit Sonawane <38550801+rstag@users.noreply.github.com>
2021-12-05 19:16:04 -0500

    Update palindrome.js

--- Quicktips/palindrome.js
+++ Quicktips/palindrome.js
@@ -1,10 +1,8 @@
 function solution(inputString) {
     let i;
     for(i=0;i<(inputString.length+1)/2-1;i++){
-        if(inputString[i]!=inputString[inputString.length-1-i]){
-            palin=true
+        if(inputString[i]!=inputString[inputString.length-1-i])
             break;
-        }
     }
     return (i>=((inputString.length+1)/2-1)?true:false)
 }
